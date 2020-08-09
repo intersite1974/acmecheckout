eb23d2581af11470363f33519544b6e7778e0ab4
James Downer <jamesdowner@sky.com>
2020-08-09 23:20:59 +0100

    Promotion repository start

---
+++ src/CheckoutKata/PromotionEngine/PromotionRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CheckoutKata.Models;
+
+namespace CheckoutKata.PromotionEngine
+{
+    public class PromotionRepository : IPromotionRepository
+    {
+        #region Fields
+
+        private IList<Promotion> _promotions;
+
+        #endregion
+
+        #region Constructor
+
+        public PromotionRepository()
+        {
+            _promotions = new List<Promotion>();
+        }
+
+        #endregion
+
+        #region IPromotionRepository
+
+        public bool AddPromotion(Promotion promotion)
+        {
+            if (promotion == null)
+                throw new ArgumentNullException(nameof(promotion));
+
+            _promotions.Add(promotion);
+            return true;
+        }
+
+        public bool TryGetPromotion(int promotionId, out Promotion promotion)
+        {
+            if (promotionId == 0)
+            {
+                promotion = null;
+                return false;
+            }
+
+            promotion = _promotions.Where(i => i.PromotionId == promotionId).FirstOrDefault();
+
+            return promotion != null;
+        }
+
+        #endregion
+    }
+}
---
+++ tests/CheckoutKata.UnitTests/PromotionEngine/PromotionRepositoryTests.cs
@@ -0,0 +1,60 @@
+﻿using Xunit;
+using FluentAssertions;
+using CheckoutKata.PromotionEngine;
+using CheckoutKata.Models;
+using System;
+
+namespace CheckoutKata.UnitTests.PromotionEngine
+{
+    public class PromotionRepositoryTests
+    {
+        #region Fields
+
+        private readonly IPromotionRepository _sut;
+
+        #endregion
+
+        #region Constructor
+
+        public PromotionRepositoryTests()
+        {
+            _sut = new PromotionRepository();
+        }
+
+        #endregion
+
+        #region Tests
+
+        [Fact]
+        public void TryingToAddANullPromotion_Throws_ArgumentNullException()
+        {
+            // Act
+            Action act = () => _sut.AddPromotion(null);
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Promotion_IsAdded_OK()
+        {
+            // Arrange
+            var promotionToBeAdded = new Promotion
+            {
+                PromotionId = 1,
+                Description = "This is a test",
+                PromotionPrice = 9.99M,
+                QuantityTrigger = 3,
+                PromotionPercentageReduction = 0.00M
+            };
+
+            // Act
+            var result = _sut.AddPromotion(promotionToBeAdded);
+
+            // Assert
+            result.Should().Be(true);
+        }
+
+        #endregion
+    }
+}
