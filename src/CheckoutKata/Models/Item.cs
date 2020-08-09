132518772985865627faddb1a61783a53661ab04
James Downer <jamesdowner@sky.com>
2020-08-09 23:03:42 +0100

    Basic models, NuGet package add ins, hook up main project into unit tests project.

---
+++ src/CheckoutKata/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace CheckoutKata.Models
+{
+    [ExcludeFromCodeCoverage]
+    public class Item
+    {
+        #region Properties
+
+        public string SkuId { get; set; }
+        public int PromotionId { get; set; }
+        public string Description { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        #endregion
+    }
+}
---
+++ src/CheckoutKata/Models/Promotion.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace CheckoutKata.Models
+{
+    [ExcludeFromCodeCoverage]
+    public class Promotion
+    {
+        #region Properties
+
+        public int PromotionId { get; set; }
+        public string Description { get; set; }
+        public int QuantityTrigger { get; set; }
+        public decimal PromotionPrice { get; set; }
+        public decimal PromotionPercentageReduction { get; set; }
+
+        #endregion
+    }
+}
