9b33f140d64e148eab858696e44c9673ba0d38f9
James Downer <jamesdowner@sky.com>
2020-08-10 01:07:21 +0100

    DiscountService plus tests

---
+++ src/CheckoutKata/PromotionEngine/DiscountService.cs
@@ -0,0 +1,19 @@
+﻿namespace CheckoutKata.PromotionEngine
+{
+    public class DiscountService : IDiscountService
+    {
+        #region IDiscountService
+
+        public decimal PlainVolumeDiscount(decimal unitPrice, decimal promotionPrice)
+        {
+            return unitPrice - (promotionPrice - unitPrice);
+        }
+
+        public decimal PercentageDiscount(int quantity, decimal unitPrice, decimal promotionPercentage)
+        {
+            return unitPrice - (promotionPercentage / 100) * (quantity * unitPrice);
+        }
+
+        #endregion
+    }
+}
---
+++ tests/CheckoutKata.UnitTests/PromotionEngine/DiscountServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Xunit;
+using NSubstitute;
+using FluentAssertions;
+using CheckoutKata.PromotionEngine;
+
+namespace CheckoutKata.UnitTests.PromotionEngine
+{
+    public class DiscountServiceTests
+    {
+        #region Constants
+
+        #endregion
+
+        #region Fields
+
+        private readonly IDiscountService _sut;
+
+        #endregion
+
+        #region Constructor
+
+        public DiscountServiceTests()
+        {
+            _sut = new DiscountService();
+        }
+
+        #endregion
+
+        #region Tests
+
+        [Theory]
+        [InlineData(8.99, 9.99, 7.99)]
+        [InlineData(6.00, 4.29, 7.71)]
+        [InlineData(19.99, 15.99, 23.99)]
+
+        public void PlainVolumeDiscount_OK(decimal unitPrice, decimal promotionPrice, decimal expectedValue)
+        {
+            // Act
+            var result = _sut.PlainVolumeDiscount(unitPrice, promotionPrice);
+
+            // Assert
+            result.Should().Be(expectedValue);
+        }
+
+
+        #endregion
+    }
+}
